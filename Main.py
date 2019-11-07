0505be0f4030d2325405270e7b6e70e8638d9b44
pranav-28 <theforumconsilio@gmail.com>
2019-11-06 23:05:20 -0800

    Updated the background and the ground with the complete animation of the bird

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
+# Flappy Bird
+
 import pygame
 
 
 def main():
-    window_size = (1080, 1920)
+    window_size = (1080, 1920)  # Full HD Resolution
 
     pygame.display.set_mode(window_size)
     pygame.display.set_caption("Flappy Bird")
@@ -14,44 +16,53 @@ def main():
     pygame.quit()
 
 
+# Main game class
 class Game:
     def __init__(self, surface):
         # game variables
-
         self.surface = surface
         self.clock = pygame.time.Clock()
         self.bg_color = pygame.Color('black')
         self.continue_game = True
+        self.start_game = False
         self.key_pressed = False
 
         # fps counter
         self.fps = 30
         self.max_fps = 60
 
-        # bird variables
-        self.color = pygame.Color('white')
-        self.bird = pygame.Rect(surface.get_width() // 2, surface.get_height() // 2, 50, 50)
-        self.bird_x_velocity = 5
-        self.isJump = False
-        self.max_jump = 10
-        self.jump_count = 0
-
         # background
         self.background = pygame.image.load("./Assets/flappy_background.png").convert()
         self.bg_scorll = 0
         self.bottom = pygame.image.load("./Assets/Bottom_border.png").convert()
 
+        # bird variables
+        self.color = pygame.Color('black')
+        self.bird = pygame.Rect(surface.get_width() // 2, surface.get_height() // 2 - self.bottom.get_rect().height, 50,
+                                50)
+        self.bird_x_velocity = 5
+
+        # Jumping variables
+        self.isJump = False
+        self.max_jump = 10
+        self.jump_count = 0
+
+        # bottom rect for collision
+        self.bottom_rect = pygame.Rect(0, self.surface.get_height() - self.bottom.get_rect().height,
+                                       self.bottom.get_rect().width, self.bottom.get_rect().height)
+
     def play(self):
+        # main game loop
         while self.continue_game:
             self.event_handler()
-
+            self.jump()
             self.draw()
-            self.update()
-
-            self.check_border()
+            self.check_bottom_collision()
+            pygame.display.update()
             self.clock.tick(self.fps)
 
     def event_handler(self):
+        # check for event in pygame
         events = pygame.event.get()
         keys = pygame.key.get_pressed()
         for event in events:
@@ -59,34 +70,43 @@ class Game:
                 self.continue_game = False
             if event.type == pygame.KEYDOWN:
                 if keys[pygame.K_SPACE]:
+                    self.start_game = True
                     self.isJump = True
                 self.key_pressed = True
             if event.type == pygame.KEYUP:
                 self.key_pressed = False
 
+    def jump(self):
+        # starts the game when space is pressed
+        if self.start_game:
+            self.jump()
+
     def draw(self):
-        self.surface.blit(self.background, (0, 0))
+        # draws the objects on the surface
+        self.surface.blit(self.background, (0, -self.bottom.get_rect().height))
         pygame.draw.rect(self.surface, self.color, self.bird)
-        self.background_scroll()
-
-
-    def update(self):
-        self.jump()
-        pygame.display.update()
+        if self.continue_game:
+            self.background_scroll()
 
     def background_scroll(self):
-
+        # infinite scroll of the ground when game is being played
         rel_x = self.bg_scorll % self.bottom.get_rect().width
-        self.surface.blit(self.bottom, (rel_x - self.bottom.get_rect().width, self.surface.get_height() - self.bottom.get_rect().height))
+        self.surface.blit(self.bottom, (
+            rel_x - self.bottom.get_rect().width, self.surface.get_height() - self.bottom.get_rect().height))
         if rel_x < self.surface.get_width():
             self.surface.blit(self.bottom, (rel_x, self.surface.get_height() - self.bottom.get_rect().height))
         self.bg_scorll -= 5
 
-    def check_border(self):
-        if self.bird.y > self.surface.get_height() + 100:
+    def check_bottom_collision(self):
+        # checking for end game collision
+        if self.bottom_rect.collidepoint(self.bird.x, self.bird.y + self.bird.height):
             self.continue_game = False
+            pygame.draw.line(self.surface, pygame.Color('red'), (0, self.bird.y + self.bird.height),
+                             (self.surface.get_width(), self.bird.y + self.bird.height))
+            pygame.display.update()
 
     def jump(self):
+        # jumping function for the bird
         if self.isJump:
             self.jump_count = self.max_jump
             self.isJump = False
